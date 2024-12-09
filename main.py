48ddc697230e621b5b685373d1ed03afb560e827
a k <ak97plutonium@gmail.com>
2024-12-09 11:38:04 +0530

    take_a_toss
    this function takes a toss for the player to play the game.

--- main.py
+++ main.py
@@ -9,16 +9,31 @@ def taking_plr_name() -> str:
     return plr_name
 
 
-def taking_lvl() -> int:
+def taking_lvl(display_name : str) -> int:
+    """taking level from player"""
     lvl = 0 # to hold lvl of the game.
     # taking lvl input.
     print("\n")
     print("======================================================")
     print("-- Level are 1, 2, 3, 4, 5 ---------------------------")
     while lvl < 1 or lvl > 5:
-        lvl = int(input("-- Please Enter your lvl :>> "))
+        lvl = int(input(f"-- {display_name.upper()}, please Enter your lvl :>> "))
     print("======================================================\n")
     return lvl
+
+
+def take_a_toss(plr_name : str) -> bool:
+    """it's a toss to decide who goes first"""
+    faces_list : list = ['0', '1']
+    player_choice : str = input("Press 0:HEADS OR 1:TAILS :>> ")
+    # checking for player choice.
+    if player_choice == faces_list[0]:
+        print(f"{plr_name.upper()}, chose HEADS")
+    elif player_choice == faces_list[1]:
+        print(f"{plr_name.upper()}, chose TAILS")
+    else:
+        print("INVALID CHOICE")
+    return player_choice == random.choice(faces_list)
 
 
 # game_onn var
@@ -27,6 +42,7 @@ game_onn : bool = True
 # while loop to run the game.
 while game_onn:
     player_name : str = taking_plr_name()  # taking player name.
+    taking_lvl(player_name)
 
     # asking if player want to play again.
     if input("Enter 'a' to play agin :>> ") != 'a':
