d26e53c2a73f3371bfe7afce950c60c505aa1cd0
a k <ak97plutonium@gmail.com>
2024-12-09 10:31:50 +0530

    taking_lvl
    function taking lvl from 1, 2, 3, 4, 5

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ def taking_plr_name() -> str:
     """function to take name from player"""
     plr_name : str = ""  # var to hold player name.
     while len(plr_name) < 3 or not plr_name.isalpha():
-        plr_name = input("Player, enter you name :>> ")
+        plr_name = input("-- Player, enter you name :>> ")
     return plr_name
+
+
+def taking_lvl() -> int:
+    lvl = 0 # to hold lvl of the game.
+    # taking lvl input.
+    print("\n")
+    print("======================================================")
+    print("-- Level are 1, 2, 3, 4, 5 ---------------------------")
+    while lvl < 1 or lvl > 5:
+        lvl = int(input("-- Please Enter your lvl :>> "))
+    print("======================================================\n")
+    return lvl
 
 
 # game_onn var
@@ -14,7 +26,8 @@ game_onn : bool = True
 
 # while loop to run the game.
 while game_onn:
+    player_name : str = taking_plr_name()  # taking player name.
 
     # asking if player want to play again.
     if input("Enter 'a' to play agin :>> ") != 'a':
-        break
+        game_onn = False
